f56c68fc0780d9d9a926fd1658bdfdbfbd41c93b
Muhammad Aulia Asshiddiqie <m.aulia08@mail.unpas.ac.id>
2019-10-02 07:13:54 +0700

    3 latihan membuat method pada produk

--- produk.php
+++ produk.php
@@ -5,6 +5,11 @@ class Produk {
 			$penulis = "penulis",
 			$penerbit = "penerbit",
 			$harga = 0;
+
+
+	public function getLabel(){
+		return "$this->penulis, $this->penerbit";
+	}
 }
 
 // $produk1 = new Produk();
@@ -24,6 +29,13 @@ $produk3->penulis =  "Boruto";
 $produk3->penerbit = " Sidiq";
 $produk3->harga = 10000;
 
-echo "Komik : $produk3->penulis, $produk3->penerbit";
+$produk4 = new Produk();
+$produk4->judul =  "Uncharted";
+$produk4->penulis =  "Neil Druckman";
+$produk4->penerbit = " Sony Computer";
+$produk4->harga = 100000;
 
+echo "Komik : ".$produk3->getLabel();
+echo "<br>";
+echo "Game : ".$produk4->getLabel();
  ?> 
